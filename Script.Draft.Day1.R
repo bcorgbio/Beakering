392f9a3c4204b9cba8d2ed4853c6b9776ead83e3
miloshj1 <83241739+miloshj1@users.noreply.github.com>
2022-12-11 17:56:10 -0500

    new updates

--- Script.Draft.Day1.R
+++ Script.Draft.Day1.R
@@ -20,3 +20,43 @@ ggplot(data.norm, aes(x = Trophic.Niche, y = w.l.ratio, color = Trophic.Niche)) 
   geom_boxplot() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   coord_cartesian(ylim = c(quantile(data.norm$w.l.ratio, 0.0), quantile(data.norm$w.l.ratio, 0.999)))
 
+data.norm <- data.norm %>% 
+  mutate(bsize.bodymass.ratio = w.l.ratio / Mass)
+data.norm$bsize.bodymass.ratio
+
+ggplot(data.norm, aes(x = Trophic.Level, y = bsize.bodymass.ratio, color = Trophic.Level)) +
+  geom_boxplot() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  coord_cartesian(ylim = c(quantile(Beak.Size.Body.Mass$bsize.bodymass.ratio
+  , 0.5), quantile(Beak.Size.Body.Mass$bsize.bodymass.ratio
+  , 0.999)))
+
+ggplot(data.norm, aes(x = w.l.ratio, y = Mass, color = Trophic.Level, type="n")) +
+  geom_point() + geom_smooth(method="lm")+
+  scale_y_continuous(trans='log10')+
+  scale_x_continuous(trans='log10') 
+
+# beak length = height of pyramid, beak depth = length of side, beak width = width of pyramid
+data
+
+
+
+ggplot(data.norm, aes(x = w.l.ratio, y = beak.volume, color = Trophic.Level, type="n")) +
+  geom_point() + geom_smooth(method="lm")+
+  scale_y_continuous(trans='log10')+
+  scale_x_continuous(trans='log10') 
+
+colnames(data.norm)
+head(data)
+colnames(data)
+
+data.norm <- data.norm %>% 
+  mutate(beak.volume = ((Beak.Length_Culmen*Beak.Depth*Beak.Width)/3)) 
+data.norm$beak.volume
+
+ggplot(data.norm, aes(x = Mass, y = beak.volume, color = Trophic.Level, type="n")) +
+  geom_point() + geom_smooth(method="lm")+
+  scale_y_continuous(trans='log10')+
+  scale_x_continuous(trans='log10') 
+
+
+
